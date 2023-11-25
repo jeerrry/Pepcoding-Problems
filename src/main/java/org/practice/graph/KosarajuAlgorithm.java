14988b3ed66a69c333df9e9ca9f161bd95aded43
senuch <s.uzairtariq@gmail.com>
2023-11-25 13:55:56 +0500

    Park Regions

--- src/main/java/org/practice/graph/KosarajuAlgorithm.java
+++ src/main/java/org/practice/graph/KosarajuAlgorithm.java
@@ -3,8 +3,7 @@ package org.practice.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Stack;
+import java.util.*;
 
 public class KosarajuAlgorithm {
     public static void main(String[] args) throws IOException {
